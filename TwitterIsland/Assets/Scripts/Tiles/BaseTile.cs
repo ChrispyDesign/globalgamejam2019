c50b8f6d8de5ef9e5d2f6bb30331a9c998859a03
Cameron Brown <cameron@cmrn.io>
2019-01-26 13:00:26 +1100

    implement tile's adjacency checks things

--- TwitterIsland/Assets/Scripts/Tiles/BaseTile.cs
+++ TwitterIsland/Assets/Scripts/Tiles/BaseTile.cs
@@ -1,19 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseTile : MonoBehaviour {
+public class BaseTile : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Debug.Log(GetAdjacentTiles().Count);
+    }
 
     public List<BaseTile> GetAdjacentTiles(int casts = 12)
     {
         List<BaseTile> result = new List<BaseTile>();
 
-        float perStep = (2.0f * Mathf.PI) / 2.0f;
-        for(float f = 0; f < 2.0f * Mathf.PI; f += perStep)
+        float perStep = (2.0f * Mathf.PI) / casts;
+        for (float f = 0; f < 2.0f * Mathf.PI; f += perStep)
         {
             // angle is f, turn that into a vector
-            //Vector3 direction = new Vector3()/*;*/
-            // no
+            Vector3 direction = new Vector3();
+            direction.x = Mathf.Sin(f);
+            direction.z = Mathf.Cos(f);
+
+            var hits = Physics.RaycastAll(transform.position, direction);
+            float closestDist = Mathf.Infinity;
+            BaseTile closestTile = null;
+            foreach(var hit in hits)
+            {
+                if(hit.distance < closestDist && hit.collider.gameObject != this.gameObject)
+                {
+                    closestDist = hit.distance;
+                    closestTile = hit.collider.gameObject.GetComponent<BaseTile>();
+                }
+            }
+            if (closestTile != null && !result.Contains(closestTile))
+                result.Add(closestTile);
         }
 
         return result;

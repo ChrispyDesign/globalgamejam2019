306e96d8c5f22b5a24ce843e4f6eedcb99d370aa
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2019-01-26 12:04:24 +1100

    Random generator really committed now

---
+++ TwitterIsland/Assets/Scripts/RandomTileGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTileGenerator : MonoBehaviour {
+
+    public List<Transform> spawnPoints;
+    public List<GameObject> tiles;
+    public List<GameObject> pieces;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        //if (pieces != null)
+        //{
+        //    foreach (var piece in pieces)
+        //    {
+        //        Destroy(piece);
+        //    }
+
+        //    pieces.Clear();
+        //}
+
+        foreach (var points in spawnPoints)
+        {
+
+            int tileNumber = Random.Range(0, tiles.Count);
+            GameObject piece = Instantiate(tiles[tileNumber], points.position, points.rotation);
+            pieces.Add(piece.gameObject);
+
+        }
+
+    }
+}

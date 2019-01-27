ec23a25dd9c286b196319a11e4482976dc2186e0
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2019-01-27 14:35:54 +1100

    SPlashScreen working

---
+++ TwitterIsland/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour
+{
+    public Image img;
+    public Image img2;
+
+    public void Start()
+    {
+        StartCoroutine("Splash_Screen");
+    }
+
+    IEnumerator Splash_Screen()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            Color c = img.color;
+            c.a = f;
+            img.color = c;
+            yield return null;
+        }
+        img.gameObject.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(2f);
+
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            Color c = img2.color;
+            c.a = f;
+            img2.color = c;
+            yield return null;
+        }
+        img2.gameObject.SetActive(false);
+    }
+}

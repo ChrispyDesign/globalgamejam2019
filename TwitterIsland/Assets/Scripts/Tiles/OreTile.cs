4fdb8b3806716f66479938dccbad23144654fbe7
Cameron Brown <cameron@cmrn.io>
2019-01-27 13:38:47 +1100

    ore tiles :)

---
+++ TwitterIsland/Assets/Scripts/Tiles/OreTile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreTile : BaseTile
+{
+    public override string GetPrefabName()
+    {
+        return "Ore";
+    }
+}

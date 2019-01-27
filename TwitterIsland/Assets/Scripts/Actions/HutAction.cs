29889e92cb5a7c1b13514efb605933472c677085
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2019-01-27 15:09:16 +1100

    IDK

---
+++ TwitterIsland/Assets/Scripts/Actions/HutAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HutAction : TileAction
+{
+    public override void Setup()
+    {
+        m_points = 100;
+        type = ActionType.TILE_ACTION;
+        conditionals.Add(() => { return GameController.worldResources["wood"] > 0; });
+    }
+
+    public override void Perform(BaseTile onTile)
+    {
+        GameController.instance.m_PlayerScore += m_points;
+        GameController.worldResources["wood"] -= 1;
+        GameController.instance.ReplaceTile(onTile, "Huts");
+    }
+}
---
+++ TwitterIsland/Assets/Scripts/Actions/PlantCropAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantCropAction : TileAction
+{
+    public override void Setup()
+    {
+        m_points = 30;
+        type = ActionType.TILE_ACTION;
+        conditionals.Add(() => { return GameController.instance.GetBuildingCount() > 0; });
+    }
+
+    public override void Perform(BaseTile onTile)
+    {
+        GameController.instance.m_PlayerScore += m_points;
+        GameController.instance.ReplaceTile(onTile, "Crops");
+    }
+}
